9da343268320c9154dcd701b8eaa99ee376bbc4d
andyp <andy.pham2956@gmail.com>
2020-10-24 02:18:15 -0400

    add environment variable

--- app.py
+++ app.py
@@ -5,25 +5,29 @@ from server.mockdraft.service.prospect_elite_service import ProspectEliteService
 
 import json
 
-
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template("index.html", page="index")
+
 
 @app.route('/prospects')
 def prospect_list():
     pagination = request.args.get('page') if request.args.get('page') is not None else 1
     return render_template("index.html", page="prospect_list", pagination=pagination)
 
+
 @app.route('/prospects/<id>')
 def prospect_page(id):
     return render_template("index.html", page="prospect", prospect_id=id)
 
+
 @app.route('/draftcenter')
 def draft_center():
     return render_template("index.html", page="draft_center")
+
 
 # -------- API Routing -------------
 @app.route('/api/drafts')
@@ -37,9 +41,9 @@ def getEntireDraftSimulation():
     # return json.dumps([draft.__dict__ for draft in draftpicks])
     return json.dumps(response, default=lambda o: o.__dict__)
 
+
 @app.route('/api/prospects')
 def getProspects():
-
     response = response = ProspectEliteService().getAllProspects()
     if request.args.get('position') is not None:
         page = request.args.get('page') if request.args.get('page') is not None else 0
@@ -50,8 +54,9 @@ def getProspects():
         response = ProspectEliteService().getProspectById(request.args.get('id'))
     else:
         response = ProspectEliteService().getAllProspects()
-    
+
     return json.dumps(response, default=lambda o: o.__dict__)
+
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support

e7d98ff6c81b2a4869dc05f0f474ca0f9d69f6b1
andyp <andy.pham2956@gmail.com>
2020-10-28 20:10:32 -0400

    add pytest & test job in github workflow

--- server/nhlapi/service/facade/nhl_roster_service_facade.py
+++ server/nhlapi/service/facade/nhl_roster_service_facade.py
@@ -1,16 +1,11 @@
-from server.nhlapi.service.nhl_player_service import NHLPlayerService
 from server.nhlapi.service.nhl_roster_service import NHLRosterService
 
 
 class NHLRosterServiceFacade:
 
-    def __init__(self, nhlRosterService=NHLRosterService(), nhlPlayerService=NHLPlayerService()):
+    def __init__(self, nhlRosterService=NHLRosterService()):
         self.nhlRosterService = nhlRosterService
-        self.nhlPlayerService = nhlPlayerService
 
     def get_nhl_roster_by_team_id(self, id):
         player_ids = self.nhlRosterService.get_team_roster_by_id(id)
         return player_ids
-
-
-print(NHLRosterServiceFacade().get_nhl_roster_by_team_id(1))

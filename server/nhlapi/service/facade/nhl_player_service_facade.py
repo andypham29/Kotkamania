2395d06e20e233a3f467740fec30b4dd085a4795
andyp <andy.pham2956@gmail.com>
2021-01-02 20:29:45 -0500

    remove repository from facade

--- server/nhlapi/service/facade/nhl_player_service_facade.py
+++ server/nhlapi/service/facade/nhl_player_service_facade.py
@@ -1,5 +1,3 @@
-from server.internaldata.repository.player_repository import InternalPlayerRepository
-from server.internaldata.repository.player_stat_repository import InternalPlayerStatRepository
 from server.nhlapi.service.nhl_player_service import NHLPlayerService
 from server.nhlapi.service.nhl_player_stat_service import NHLPlayerStatService
 
@@ -7,13 +5,9 @@ from server.nhlapi.service.nhl_player_stat_service import NHLPlayerStatService
 class NHLPlayerServiceFacade:
 
     def __init__(self, nhlPlayerService=NHLPlayerService(),
-                 nhlPlayerStatService=NHLPlayerStatService(),
-                 internalPlayerRepository=InternalPlayerRepository(),
-                 internalPlayerStatRepository=InternalPlayerStatRepository()):
+                 nhlPlayerStatService=NHLPlayerStatService()):
         self.nhlPlayerService = nhlPlayerService
         self.nhlPlayerStatService = nhlPlayerStatService
-        self.internalPlayerRepository = internalPlayerRepository
-        self.internalPlayerStatRepository = internalPlayerStatRepository
 
     def get_player_by_playerId_and_seasons(self, playerId, seasons=[""]):
         # seasons = ["20152016", "20162017", "20172018"]
@@ -26,16 +20,3 @@ class NHLPlayerServiceFacade:
         player.stats = stats
 
         return player
-
-    def get_skater_by_playerId_and_seasons_internally(self, playerId):
-        player = self.internalPlayerRepository.get_player_by_id(playerId)
-        stats = self.internalPlayerStatRepository.get_internal_player_stats_by_playerId(playerId)
-
-        player.stats = stats
-
-        return player
-
-    def get_skaters_and_seasons_internally(self):
-        players = self.internalPlayerRepository.get_all_players()[:100]
-
-        return players

d66f3cd8ffbf63490e13d7ad04e94230ff972e33
andyp <andy.pham2956@gmail.com>
2021-10-27 11:46:29 -0400

    add calendar helper

---
+++ server/commons/helper/calendar_helper.py
@@ -0,0 +1,23 @@
+from datetime import timedelta, datetime
+
+
+class CalendarHelper:
+
+    @staticmethod
+    def get_current_day():
+        return datetime.now().date()
+
+    @staticmethod
+    def get_current_week_monday(today=datetime.now().date()):
+        monday = today - timedelta(days=today.weekday())
+        return monday
+
+    @staticmethod
+    def get_current_week_sunday(today=datetime.now().date()):
+        sunday = today - timedelta(days=today.weekday() - 6)
+        return sunday
+
+
+if __name__ == '__main__':
+    print(CalendarHelper.get_current_week_monday())
+    print(CalendarHelper.get_current_week_sunday())
